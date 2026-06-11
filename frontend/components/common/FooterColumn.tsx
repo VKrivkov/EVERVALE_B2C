1dcfe833bb16a4a0ad828be6c3c6e631d22b51ad
Yaroslav <166743450+yaroslavmuzalevskyi@users.noreply.github.com>
2026-06-11 11:51:29 +0200

    updated HELP/FAQ link

--- frontend/components/common/FooterColumn.tsx
+++ frontend/components/common/FooterColumn.tsx
@@ -12,23 +12,33 @@ type FooterColumnProps = {
   className?: string;
 };
 
+function isExternal(href: string) {
+  return /^(mailto:|tel:|https?:\/\/|sms:)/i.test(href);
+}
+
 export default function FooterColumn({
   title,
   links,
   className,
 }: FooterColumnProps) {
+  const linkClass =
+    "inline-flex w-full justify-center text-center lg:w-auto lg:justify-start lg:text-left hover:text-pr_w transition";
+
   return (
     <div className={cn("flex flex-col gap-3 text-sm", className)}>
       <p className="text-pr_w/90 font-semibold">{title}</p>
       <ul className="flex flex-col gap-2 text-pr_w/70">
         {links.map((link) => (
           <li key={`${title}-${link.label}`}>
-            <Link
-              href={link.href}
-              className="inline-flex w-full justify-center text-center lg:w-auto lg:justify-start lg:text-left hover:text-pr_w transition"
-            >
-              {link.label}
-            </Link>
+            {isExternal(link.href) ? (
+              <a href={link.href} className={linkClass}>
+                {link.label}
+              </a>
+            ) : (
+              <Link href={link.href} className={linkClass}>
+                {link.label}
+              </Link>
+            )}
           </li>
         ))}
       </ul>
